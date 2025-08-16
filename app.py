80a3f1615aa29c855f8eae0f7035a80ca73c88d3
vo viet <90944284+hoangviet24@users.noreply.github.com>
2025-08-16 19:20:14 +0700

    Fix requirments.txt

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def download():
 if __name__ == "__main__":
     if not os.path.exists("downloads"):
         os.makedirs("downloads")
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
